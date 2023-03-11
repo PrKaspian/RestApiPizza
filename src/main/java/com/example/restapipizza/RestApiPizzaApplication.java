7472af21453f66b410cde6a01559ac3329721bf1
PrKaspian <m.lisitsa2016@gmail.com>
2023-03-11 19:10:16 +0200

    Initial commit

---
+++ src/main/java/com/example/restapipizza/RestApiPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapipizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiPizzaApplication.class, args);
+    }
+
+}
